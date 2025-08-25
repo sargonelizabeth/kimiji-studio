153a1d2ab73bba5f9fb45479a8b09a786a140775
Sargon Elizabeth <sargonelizabeth@Sargonui-MacBookPro.local>
2025-08-25 15:56:28 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-5xl font-bold text-indigo-600">
+        Hello World 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
